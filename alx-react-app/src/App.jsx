c9c0299aea03a0fc8f6c224f3b99342174beb36c
iradukunda patrick <paddy.irad@gmail.com>
2024-12-29 09:47:31 +0200

    add component to my project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +10,7 @@ function App() {
   return (
     <>
       <div>
-          {/* WelcomeMessage Component */}
-          <WelcomeMessage />
-
-{/* Existing Content */}
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
