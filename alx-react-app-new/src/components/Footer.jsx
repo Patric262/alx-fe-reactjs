b3e41ff24bdba616a6435aa4ccbd66ffc6df982c
iradukunda patrick <paddy.irad@gmail.com>
2024-12-30 15:38:52 +0200

    Fix: Add required inline styles to Header component

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,11 @@
-const Footer = () => {
-    return (
-      <footer>
-        <p>2023 City Lovers</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>&copy; 2024 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
