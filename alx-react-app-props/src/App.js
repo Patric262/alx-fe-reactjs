7022794538a33c3f723922361d8dbde673b59bb5
iradukunda patrick <paddy.irad@gmail.com>
2024-12-30 21:27:02 +0200

    Replace prop drilling with Context API for user data management

---
+++ alx-react-app-props/src/App.js
@@ -0,0 +1,4 @@
+import WelcomeMessage from "./components/WelcomeMessage.jsx"
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
