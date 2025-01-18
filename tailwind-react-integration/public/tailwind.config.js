5efc8fb3229f25317824a71645d3bac612113cd2
iradukunda patrick <paddy.irad@gmail.com>
2025-01-18 21:56:12 +0200

    up date tailwind.concig.js

--- tailwind-react-integration/public/tailwind.config.js
+++ tailwind-react-integration/public/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // For older versions
+    darkMode: false, // Disable dark mode (or set to 'media' or 'class')
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // For Tailwind v3+
     theme: {
+      extend: {},
+    },
+    variants: {
       extend: {},
     },
     plugins: [],
