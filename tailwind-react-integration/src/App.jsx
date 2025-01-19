2c17f91b9154267b22edc742cb67b470f2fa4923
iradukunda patrick <paddy.irad@gmail.com>
2025-01-19 17:52:29 +0200

    create userprofile.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
+import UserProfile from "./components/UserProfile";
 export default function App() {
   return (
     <div className="text-center text-red-500 text-2xl font-bold">
-      Tailwind CSS is working!
+      <UserProfile />
     </div>
   );
+  
 }
+
